c296d4048021b73c1d56735649f5ec1e1d3f817d
Andre Gomes <andre.gomes@univ-lille1.fr>
2020-09-03 00:03:01 +0200

    added methods to tensor, added contraction module
    split modules to tet programs

---
+++ src/test_contraction.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# toolset to parse diagen output
+# Andre Gomes <andre.gomes@univ-lille.fr>
+
+import contraction as c
+
+def main():
+
+   c1 = c.binary_contraction()
+   string = "Z49(e1b|m1b)+=H17(e1b,l1b|d1bd2b)*S44(d1bd2b|m1b,l1b)"
+   print("\ninput  : ",string)
+   c1.parse_contraction(string, verbose=True)
+   c1.process_contraction(split_groups=True,remove_bar=True,verbose=True)
+#  print("output : ")
+#  rct1.print_contraction()
+
+
+main()
+
