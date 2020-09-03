05a3757c8c9923a32c94fdb64bc8934d3a0f83ad
Andre Gomes <andre.gomes@univ-lille1.fr>
2020-09-03 23:22:23 +0200

    saving work on defining contraction regexp
    now also parse and store the operations.
    
    added more tests strings to parse, have to check whether factors
    are being set correctly for indices summed over

--- src/test_contraction.py
+++ src/test_contraction.py
@@ -15,6 +15,18 @@ def main():
    print("output : ",expr)
 #  c1.print_contraction()
 
+   c2 = c.binary_contraction()
+   string = "Z48(|)+=S44(d1bd2b|l1bl2b)*H24(l1bl2b|d1bd2b)"
+   print("\ninput  : ",string)
+   c2.parse_contraction(string, verbose=True)
+   expr = c2.process_contraction(split_groups=True,remove_bar=True,verbose=True)
+   print("output : ",expr)
 
+   c3 = c.binary_contraction()
+   string = "Z56_1(l1b|m1b)+=H25(l1b,l2b|m1b,d1b)*S43(d1b|l2b)*-1."
+   print("\ninput  : ",string)
+   c3.parse_contraction(string, verbose=True)
+   expr = c3.process_contraction(split_groups=True,remove_bar=True,verbose=True)
+   print("output : ",expr)
 main()
 
