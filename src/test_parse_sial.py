1e223c42abe4e7dd294880e577fa965cce4c5898
Andre Gomes <andre.gomes@univ-lille1.fr>
2020-10-22 23:18:42 +0200

    renaming methods

--- src/test_parse_sial.py
+++ src/test_parse_sial.py
@@ -3,15 +3,15 @@
 # toolset to parse diagen output
 # Andre Gomes <andre.gomes@univ-lille.fr>
 
-import parse_sial as p
+import sial as s
 
 def main():
 
    print("\nSplitting all groups, replacing all bars")
 
-   ps1 = p.sial()
-   ps1.read_input("CCSD.1.fac.u.ormo.txt")
-   ps1.parse_input(verbose=True)
+   sial_input = s.sial()
+   sial_input.read("CCSD.1.fac.u.ormo.txt")
+   sial_input.parse(verbose=True)
 
 main()
 
