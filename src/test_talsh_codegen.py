1c16482cab12f2387cc4404f714f6bb4065edef6
Andre Gomes <andre.gomes@univ-lille1.fr>
2022-04-22 14:22:41 +0200

    updated reference generated filesB
    renamed files from F to F90 (since we really generate non-F77
    code), fixed wrong type definitions and lack of modules etc

--- src/test_talsh_codegen.py
+++ src/test_talsh_codegen.py
@@ -10,32 +10,32 @@ def main():
    sial_input = tcg.TALSHcodeGenerator()
    sial_input.read_sial("CCSD.1.fac.u.ormo.txt",verbose=True)
    sial_input.generate_code()
-   sial_input.print_code(filename="auto_ccsd.F")
+   sial_input.print_code(filename="auto_ccsd.F90")
 
    sial_input = tcg.TALSHcodeGenerator(spinorbital_out=True)
    sial_input.read_sial("CCD.1.fac.u.ormo.txt",verbose=True)
    sial_input.generate_code()
-   sial_input.print_code(filename="auto_ccd_spinor.F")
+   sial_input.print_code(filename="auto_ccd_spinor.F90")
 
    sial_input = tcg.TALSHcodeGenerator(spinorbital_out=True)
    sial_input.read_sial("CCSD.1.fac.u.ormo.txt",verbose=True)
    sial_input.generate_code()
-   sial_input.print_code(filename="auto_ccsd_spinor.F")
+   sial_input.print_code(filename="auto_ccsd_spinor.F90")
 
    sial_input = tcg.TALSHcodeGenerator(spinorbital_out=True)
    sial_input.read_sial("CCSDT.1.fac.u.ormo.txt",verbose=True)
    sial_input.generate_code()
-   sial_input.print_code(filename="auto_ccsdt_spinor.F")
+   sial_input.print_code(filename="auto_ccsdt_spinor.F90")
 
-#   sial_input = tcg.TALSHcodeGenerator(spinorbital_out=True)
-#   sial_input.read_sial("CCSDTQ.1.fac.u.ormo.txt",verbose=True)
-#   sial_input.generate_code()
-#   sial_input.print_code(filename="auto_ccsdtq_spinor.F")
-#
-#   sial_input = tcg.TALSHcodeGenerator(spinorbital_out=True)
-#   sial_input.read_sial("CCSDTQ5.1.fac.u.ormo.txt",verbose=True)
-#   sial_input.generate_code()
-#   sial_input.print_code(filename="auto_ccsdtq5_spinor.F")
+   sial_input = tcg.TALSHcodeGenerator(spinorbital_out=True)
+   sial_input.read_sial("CCSDTQ.1.fac.u.ormo.txt",verbose=True)
+   sial_input.generate_code()
+   sial_input.print_code(filename="auto_ccsdtq_spinor.F90")
+
+   sial_input = tcg.TALSHcodeGenerator(spinorbital_out=True)
+   sial_input.read_sial("CCSDTQ5.1.fac.u.ormo.txt",verbose=True)
+   sial_input.generate_code()
+   sial_input.print_code(filename="auto_ccsdtq5_spinor.F90")
 
 main()
 
