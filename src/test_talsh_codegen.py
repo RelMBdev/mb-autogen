39279c78bf670d9ceab781a87881bc2ec08b1f98
Andre Gomes <andre.gomes@univ-lille1.fr>
2022-04-22 11:24:19 +0200

    adding ccd generation

--- src/test_talsh_codegen.py
+++ src/test_talsh_codegen.py
@@ -13,6 +13,11 @@ def main():
    sial_input.print_code(filename="auto_ccsd.F")
 
    sial_input = tcg.TALSHcodeGenerator(spinorbital_out=True)
+   sial_input.read_sial("CCD.1.fac.u.ormo.txt",verbose=True)
+   sial_input.generate_code()
+   sial_input.print_code(filename="auto_ccd_spinor.F")
+
+   sial_input = tcg.TALSHcodeGenerator(spinorbital_out=True)
    sial_input.read_sial("CCSD.1.fac.u.ormo.txt",verbose=True)
    sial_input.generate_code()
    sial_input.print_code(filename="auto_ccsd_spinor.F")
@@ -21,6 +26,16 @@ def main():
    sial_input.read_sial("CCSDT.1.fac.u.ormo.txt",verbose=True)
    sial_input.generate_code()
    sial_input.print_code(filename="auto_ccsdt_spinor.F")
+
+#   sial_input = tcg.TALSHcodeGenerator(spinorbital_out=True)
+#   sial_input.read_sial("CCSDTQ.1.fac.u.ormo.txt",verbose=True)
+#   sial_input.generate_code()
+#   sial_input.print_code(filename="auto_ccsdtq_spinor.F")
+#
+#   sial_input = tcg.TALSHcodeGenerator(spinorbital_out=True)
+#   sial_input.read_sial("CCSDTQ5.1.fac.u.ormo.txt",verbose=True)
+#   sial_input.generate_code()
+#   sial_input.print_code(filename="auto_ccsdtq5_spinor.F")
 
 main()
 
