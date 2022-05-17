0223a81198763be5ca22a6b2898e3cdfa6a9b761
Andre Gomes <andre.gomes@univ-lille1.fr>
2022-05-17 17:11:11 +0200

    changing output names for generated code

--- src/test_talsh_codegen.py
+++ src/test_talsh_codegen.py
@@ -10,32 +10,32 @@ def main():
    sial_input = tcg.TALSHcodeGenerator()
    sial_input.read_sial("CCSD.1.fac.u.ormo.txt",verbose=True)
    sial_input.generate_code()
-   sial_input.print_code(filename="auto_ccsd.F90")
+   sial_input.print_code(filename="talsh_auto_ccsd.F90")
 
    sial_input = tcg.TALSHcodeGenerator(spinorbital_out=True)
    sial_input.read_sial("CCD.1.fac.u.ormo.txt",verbose=True)
    sial_input.generate_code()
-   sial_input.print_code(filename="auto_ccd_spinor.F90")
+   sial_input.print_code(filename="talsh_auto_ccd_spinor.F90")
 
    sial_input = tcg.TALSHcodeGenerator(spinorbital_out=True)
    sial_input.read_sial("CCSD.1.fac.u.ormo.txt",verbose=True)
    sial_input.generate_code()
-   sial_input.print_code(filename="auto_ccsd_spinor.F90")
+   sial_input.print_code(filename="talsh_auto_ccsd_spinor.F90")
 
    sial_input = tcg.TALSHcodeGenerator(spinorbital_out=True)
    sial_input.read_sial("CCSDT.1.fac.u.ormo.txt",verbose=True)
    sial_input.generate_code()
-   sial_input.print_code(filename="auto_ccsdt_spinor.F90")
+   sial_input.print_code(filename="talsh_auto_ccsdt_spinor.F90")
 
    sial_input = tcg.TALSHcodeGenerator(spinorbital_out=True)
    sial_input.read_sial("CCSDTQ.1.fac.u.ormo.txt",verbose=True)
    sial_input.generate_code()
-   sial_input.print_code(filename="auto_ccsdtq_spinor.F90")
+   sial_input.print_code(filename="talsh_auto_ccsdtq_spinor.F90")
 
    sial_input = tcg.TALSHcodeGenerator(spinorbital_out=True)
-   sial_input.read_sial("CCSDTQ5.1.fac.u.ormo.txt",verbose=True)
+   sial_input.read_sial("CCSDTQP.1.fac.u.ormo.txt",verbose=True)
    sial_input.generate_code()
-   sial_input.print_code(filename="auto_ccsdtq5_spinor.F90")
+   sial_input.print_code(filename="talsh_auto_ccsdtqp_spinor.F90")
 
 main()
 
